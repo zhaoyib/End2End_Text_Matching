282ae2cd9afcb13b02206bd2bae33c943f08faab
赵艺博 <90241849+zhaoyib@users.noreply.github.com>
2024-03-19 11:03:44 +0800

    Update Reranker_preprocess.py
    add annotations.

--- toolkits/Reranker_preprocess.py
+++ toolkits/Reranker_preprocess.py
@@ -1,9 +1,10 @@
 '''
-@File    :   Reranker_preprocess.py
-@Time    :   2024/03/12 17:34:51
-@Author  :   YiboZhao 
-@Version :   1.0
-@Site    :   https://github.com/zhaoyib
+@File      :   Reranker_preprocess.py
+@Time      :   2024/03/12 17:34:51
+@LastEdit  :   2024/03/19 09:14:56
+@Author    :   YiboZhao 
+@Version   :   1.0
+@Site      :   https://github.com/zhaoyib
 '''
 from typing import List, Dict, Tuple, Type, Union
 from copy import deepcopy
@@ -12,8 +13,22 @@ def reranker_tokenizer_preproc(
         query:str, passages:List[str], tokenizer = None,
         max_length:int = 512, overlap_tokens:int = 80
         ):
+    '''
+    process the query and condidates.input query as str and passages as list of str.
+    output the texts merging query and each passage. raw text and ids.
+
+    parameter:
+        query     : str, no longer than 200 tokens.
+        passages  : list of str, which means the CVs chosen by rough retrieval.
+        tokenizer : the tokenizer loaded from pretrained model.
+        max_length: the max input length of model.
+        overlap_tokens: how may tokens to overlap. 
+    return:
+        res_merge_inputs      : the list of merged text.
+        res_merge_inputs_pids : the list of merged text, ids instead of raw text.
+    '''
     assert tokenizer is not None, "Please Provide A Valid Tokenizer for Tokenization!"
-    sep_id = tokenizer.sep_token_id
+    sep_id = tokenizer.sep_token_id #get the id of <sep>
 
     def _merge_inputs(chunk1_raw, chunk2):
         chunk1 = deepcopy(chunk1_raw)
@@ -27,9 +42,10 @@ def reranker_tokenizer_preproc(
         return chunk1
     
     query_inputs = tokenizer.encode_plus(query, truncation=False, padding=False)
-    print(query_inputs)
+    #print("query inputs from reranker preprocess",query_inputs)
+    #->{'input_ids':……,'attention_mask':……}
     max_passage_inputs_length = max_length - len(query_inputs['input_ids']) - 1
-    #max length - length of query and len of one simple seq token.
+    #max length - length of query and len of one simple seq token(mentioned at line 16).
     assert max_passage_inputs_length > 100, "Your query is too long! Please make sure your query less than 400 tokens!"
     #512 tokens in total. can't allow the query more than 400.
     overlap_tokens_implt = min(overlap_tokens, max_passage_inputs_length//4)
