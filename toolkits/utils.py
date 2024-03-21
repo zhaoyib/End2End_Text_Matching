b5ad4258daec51ad6c953afdf64ccfce1a3b47f2
赵艺博 <90241849+zhaoyib@users.noreply.github.com>
2024-03-21 10:19:03 +0800

    Update utils.py

--- toolkits/utils.py
+++ toolkits/utils.py
@@ -7,7 +7,7 @@ def file_reader(folder_path,batch_size = 400):
     parameter: folder_path: the path of the files' folder.
                batch_size : how many files visit once.
     WARNING  : To Avoid Being Out of Memory, Don't Set the Batch Size TOO BIG!
-    return   : A dict of the Text, the key is the cv_id, the value is the texts. 
+    return   : A dict of the Text, the key is the text_id, the value is the texts. 
     '''
     items = os.listdir(folder_path)
     total_files = len(items)
@@ -19,7 +19,7 @@ def file_reader(folder_path,batch_size = 400):
             item_path = os.path.join(folder_path, items[j])
             with open(item_path,'r') as f:
                 content = f.read()
-            cv_id = items[j].split('_')[1].split('.')[0]
-            res.update({cv_id:content})
+            text_id = items[j].split('_')[1].split('.')[0]
+            res.update({text_id:content})
         if res:
             yield res
